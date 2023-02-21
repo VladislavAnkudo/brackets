e7b89f1b7361726ad16b8f94338d6c56afbbaf8b
VladislavAnkudo <ankudo47@gmail.com>
2023-02-21 12:54:28 +0300

    add file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = ['(', '{', '|', '1' ]
+  let stack = [];
+  for(let i = 0; i < str.length; i++){
+    let curSimb = str[i];
+
+    if(open.includes(curSimb)){
+      stack.push(curSimb)
+    } else {
+      if(stack.length === 0){
+        return false
+      } 
+      let elem = stack[stack.length - 1];
+
+      if(bracketsConfig[curSimb] === elem){
+        stack.pop()
+      }else{
+        return true
+      }
+    }
+  }
+  return stack.length === 0;
 }
